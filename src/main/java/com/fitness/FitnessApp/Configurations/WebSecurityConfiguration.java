094c7c32eb16beffda59b1c95a5f7b100499d98b
rickypatel5112 <rickypatel5112@gmail.com>
2023-03-23 22:35:21 -0400

    added login and logout url

--- src/main/java/com/fitness/FitnessApp/Configurations/WebSecurityConfiguration.java
+++ src/main/java/com/fitness/FitnessApp/Configurations/WebSecurityConfiguration.java
@@ -3,10 +3,9 @@ package com.fitness.FitnessApp.Configurations;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @Configuration
-@EnableOAuth2Client
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     protected void configure(HttpSecurity http) throws Exception {
@@ -14,12 +13,18 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/", "/css/**")
+                .antMatchers( "/", "/css/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2Login()
-                .permitAll();
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/");
     }
 }
