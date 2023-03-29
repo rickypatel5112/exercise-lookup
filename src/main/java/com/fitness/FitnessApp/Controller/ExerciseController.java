8960dd82ce6e27476f38c4b35d10cf12e7ece308
rickypatel5112 <rickypatel5112@gmail.com>
2023-03-28 20:05:44 -0400

    added /username endpoint for every user

--- src/main/java/com/fitness/FitnessApp/Controller/ExerciseController.java
+++ src/main/java/com/fitness/FitnessApp/Controller/ExerciseController.java
@@ -4,11 +4,12 @@ import com.fitness.FitnessApp.Model.Exercise;
 import com.fitness.FitnessApp.Service.ExerciseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ExerciseController {
@@ -17,7 +18,7 @@ public class ExerciseController {
     private ExerciseService exerciseService;
 
     @GetMapping("/")
-    public String getHomePage(){
+    public String getHomePage() {
         return "home";
     }
 
@@ -26,28 +27,45 @@ public class ExerciseController {
                               @RequestParam(name = "exType", defaultValue = "") String exType,
                               @RequestParam(name = "muscle", defaultValue = "") String muscle,
                               @RequestParam(name = "difficulty", defaultValue = "") String difficulty,
-                              @NonNull Model model){
+                              @NonNull Model model) {
 
-                Exercise[] exercises = exerciseService.getExercises(exName, exType, muscle, difficulty);
-                model.addAttribute("exercises", exercises);
+        Exercise[] exercises = exerciseService.getExercises(exName, exType, muscle, difficulty);
+        model.addAttribute("exercises", exercises);
 
-                return "search";
-    }
-
-    @GetMapping("/search")
-    public String showHtml(){
         return "search";
     }
 
-//    @GetMapping("/{userName}")
-//    public String ggf(OAuth2AuthenticationToken oAuth2AuthenticationToken){
-//
-//        String email = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("email").toString();
-//
-//        String userName = email.split("@")[0];
-//
-//        System.out.println(userName);
-//
-//        return "dashboard";
-//    }
+    @GetMapping("/search")
+    public String showHtml() {
+        return "search";
+    }
+
+    @GetMapping("/callback")
+    public String getEmail(OAuth2AuthenticationToken oAuth2AuthenticationToken) {
+
+        String email = oAuth2AuthenticationToken.getPrincipal().getAttributes().get("email").toString();
+
+        String userName = email.split("@")[0];
+        return "redirect:/user/" + userName;
+    }
+
+    @GetMapping("/user/{userName}")
+    public String userSavedItems(@PathVariable("userName") String userName, Model model) {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            String authUserName = authentication.getName().split("@")[0];
+
+            if (userName.equals(authUserName)) {
+                return "saved-items";
+            } else {
+
+                return "error";
+            }
+        } else {
+            return "redirect:/login";
+        }
+    }
+
 }
