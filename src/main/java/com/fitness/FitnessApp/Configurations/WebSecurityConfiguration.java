715d0c8b8dc8b0f97e87e314506638eaa1aa62d6
rickypatel5112 <rickypatel5112@gmail.com>
2023-07-03 20:27:29 -0400

    changed WebSecurityConfigurerAdapter (deprecated) to SecurityFilterChain

--- src/main/java/com/fitness/FitnessApp/Configurations/WebSecurityConfiguration.java
+++ src/main/java/com/fitness/FitnessApp/Configurations/WebSecurityConfiguration.java
@@ -1,24 +1,26 @@
 package com.fitness.FitnessApp.Configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
 import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration{
 
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    protected SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf()
                 .disable()
-                .authorizeRequests()
+                .authorizeHttpRequests()
                 .antMatchers("/", "/css/**", "/oauth/**")
                 .permitAll()
                 .anyRequest()
@@ -34,6 +36,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/")
                 .clearAuthentication(true);
+        return http.build();
     }
 
     private OAuth2UserService<OAuth2UserRequest, OAuth2User> oauth2UserService() {
@@ -41,7 +44,6 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
         return request -> {
             OAuth2User user = delegate.loadUser(request);
-//            String email = (String) user.getAttributes().get("email");
             return new DefaultOAuth2User(user.getAuthorities(), user.getAttributes(), "email");
         };
     }
